e96d8537f50f50fc766ea2184ff613cc6c9c78f1
Vinith69 <ramarock09@gmial.com>
2020-12-16 08:54:47 +0530

    Some additional Changes almost done and security fix

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
 const multer = require("multer");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "kalajagattu_creations",
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
 });
 
 var storage = multer.diskStorage({
@@ -31,14 +32,15 @@ const PORT = process.env.PORT || 5000;
 // Video post
 app.post("/addLinks", (req, res) => {
 	const { link, title, description } = req.body;
-	console.log(link);
+	// console.log(link);
 	connection.query(
 		`Insert into video (link,title,description) values(?,?,?)`,
 		[link, title, description],
 		(errors, results) => {
 			try {
-				if (!req.body) return errors;
-
+				if (!req.body) return res.json(errors);
+				if (Object.keys(req.body).length === 0)
+					return res.json("Empty");
 				res.send(results);
 			} catch (errors) {
 				res.send(errors);
@@ -68,9 +70,15 @@ app.post("/addImage", upload.single("myFile"), (req, res) => {
 		`Insert into image (image,title,description,date) values(?,?,?,?)`,
 		[req.file.filename, title, description, date],
 		(error, results) => {
-			if (!req.body) return res.send(errors);
-			// console.log(results);
-			res.send(results);
+			try {
+				if (!req.body) return res.send(errors);
+				if (Object.keys(req.body).length === 0)
+					return res.json("Empty");
+				// console.log(results);
+				res.send(results);
+			} catch (errors) {
+				res.send(errors);
+			}
 		}
 	);
 });
@@ -81,6 +89,17 @@ app.get("/addImage", (req, res) => {
 		if (!req.body) res.json("Error no body");
 		res.send(results);
 	});
+});
+
+app.get("/homeProgramImage", (req, res) => {
+	connection.query(
+		`select  * from image limit 3`,
+		// `select  * from image order by id desc limit 3`,
+		(errors, results) => {
+			if (!req.body) res.json("Error no body");
+			res.send(results);
+		}
+	);
 });
 
 app.get("/specificImage", (req, res) => {
